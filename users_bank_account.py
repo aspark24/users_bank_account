ebc3f2f8a7d48f09824407a8c0676486f6529263
Andrew Park <andrewpark41299@gmail.com>
2023-01-24 18:07:12 -0800

    commit

---
+++ users_bank_account.py
@@ -0,0 +1,74 @@
+# bank account class
+class BankAccount:
+    accounts = []
+
+    def __init__(self, int_rate, balance):
+        self.int_rate = int_rate
+        self.balance = balance
+        BankAccount.accounts.append(self)
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self, amount):
+        if (self.balance - amount) >= 0:
+            self.balance -= amount
+        else:
+            print("Insufficient Funds: Charging a $5 fee")
+            self.balance -= 5
+        return self
+
+    def display_account_info(self):
+        print(f"Balance: {self.balance}")
+        return self
+
+    def yield_interest(self):
+        if self.balance > 0:
+            self.balance += (self.balance * self.int_rate)
+        return self
+
+    @classmethod
+    def print_all_accounts(cls):
+        for account in cls.accounts:
+            account.display_account_info()
+
+
+savings = BankAccount(.05, 1000)
+checking = BankAccount(.02, 5000)
+
+savings.deposit(10).deposit(20).deposit(40).withdraw(
+    600).yield_interest().display_account_info()
+checking.deposit(100).deposit(200).deposit(400).withdraw(
+    60).yield_interest().display_account_info()
+
+BankAccount.print_all_accounts()
+
+
+# user class
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account = BankAccount(int_rate=0.02, balance=0)
+
+    def make_deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def make_withdrawal(self, amount):
+        if (self.balance - amount) >= 0:
+            self.balance -= amount
+        else:
+            print("Insufficient Funds: Charging a $5 fee")
+            self.balance -= 5
+        return self
+
+    def display_user_balance(self):
+        print(f"Balance: {self.balance}")
+        return self
+
+    def example_method(self):
+        # we can call the BankAccount instance's methods
+        self.account.deposit(100)
+        print(self.account.balance)		# or access its attributes
